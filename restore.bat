5fd6bc3e3d984c7d07a6d885348b4cb93e519798
bga <bga.email@gmail.com>
2021-12-13 13:59:54 +0300

    + ignore `_*`

--- restore.bat
+++ restore.bat
@@ -1,2 +1,2 @@
-robocopy "%~dp0.\." "%appdata%\Opera\Opera" /e /xd ".git" "priv"
+robocopy "%~dp0.\." "%appdata%\Opera\Opera" /e /xd ".git" "priv" _*
 echo find . -maxdepth 1 -mindepth 1 -not -name "priv" -exec rm -rf {} ';' > "%appdata%\Opera\Opera\cleanUp.sh"
