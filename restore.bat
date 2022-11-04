88046615e8217b55c788f55bfb0d5cba4a49daf1
bga <bga.email@gmail.com>
2022-11-04 23:36:26 +0300

    + ignore bak files and dirs

--- restore.bat
+++ restore.bat
@@ -1,2 +1,2 @@
-robocopy "%~dp0.\." "%appdata%\Opera\Opera" /e /xd ".git" "priv" _*
+robocopy "%~dp0.\." "%appdata%\Opera\Opera" /e /xd ".git" "priv" _* *.bak.* /xf *.bak.*
 echo find . -maxdepth 1 -mindepth 1 -not -name "priv" -exec rm -rf {} ';' > "%appdata%\Opera\Opera\cleanUp.sh"
